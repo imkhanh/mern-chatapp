a64b9abf43de1055065876e73643b232dc5e0aeb
Khanh Nguyen <nqkhanh357@gmail.com>
2023-01-29 20:34:42 +0700

    first commit

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -41,65 +41,66 @@ const LoginForm = () => {
     setFormData({ email: 'guest@gmail.com', password: '123123' });
   };
 
-  if (formData.loading) return <Loader />;
-
   return (
-    <form onSubmit={handleLogin} className="space-y-6">
-      <div>
-        <label htmlFor="email" className="block mb-1 font-medium text-gray-900">
-          Email Address
-        </label>
-        <input
-          type="text"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Please enter your email address"
-          className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
+    <div>
+      <form onSubmit={handleLogin} className="space-y-6">
+        <div>
+          <label htmlFor="email" className="block mb-1 font-medium text-gray-900">
+            Email Address
+          </label>
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Please enter your email address"
+            className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
           border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 
           duration-150 ease-linear outline-none rounded-md"
-        />
-      </div>
-      <div>
-        <label htmlFor="password" className="block mb-1 font-medium text-gray-900">
-          Password
-        </label>
-        <div className="relative">
-          <input
-            type={formData.showPassword ? 'text' : 'password'}
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder="Please enter your password"
-            className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
+          />
+        </div>
+        <div>
+          <label htmlFor="password" className="block mb-1 font-medium text-gray-900">
+            Password
+          </label>
+          <div className="relative">
+            <input
+              type={formData.showPassword ? 'text' : 'password'}
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Please enter your password"
+              className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
             border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 
             duration-150 ease-linear outline-none rounded-md"
-          />
+            />
+            <button
+              type="button"
+              onClick={() => setFormData({ ...formData, showPassword: !formData.showPassword })}
+              className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            >
+              {formData.showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </button>
+          </div>
+        </div>
+        <div className="flex gap-3">
+          <button
+            type="submit"
+            className="inline-block w-full h-11 font-medium rounded-md transition border border-blue-500 text-white bg-blue-600 hover:bg-blue-500 hover:ring-2 hover:ring-blue-200 hover:ring-offset-2"
+          >
+            Login
+          </button>
           <button
             type="button"
-            onClick={() => setFormData({ ...formData, showPassword: !formData.showPassword })}
-            className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            onClick={handleGuestLogin}
+            className="inline-block w-full h-11 font-medium rounded-md transition border border-white hover:border-blue-500 text-blue-500 hover:text-blue-600 hover:ring-2 hover:ring-blue-200 hover:ring-offset-2"
           >
-            {formData.showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            Guest
           </button>
         </div>
-      </div>
-      <div className="flex gap-3">
-        <button
-          type="submit"
-          className="inline-block w-full h-11 font-medium rounded-md transition border border-blue-500 text-white bg-blue-600 hover:bg-blue-500 hover:ring-2 hover:ring-blue-200 hover:ring-offset-2"
-        >
-          Login
-        </button>
-        <button
-          type="button"
-          onClick={handleGuestLogin}
-          className="inline-block w-full h-11 font-medium rounded-md transition border border-white hover:border-blue-500 text-blue-500 hover:text-blue-600 hover:ring-2 hover:ring-blue-200 hover:ring-offset-2"
-        >
-          Guest
-        </button>
-      </div>
-    </form>
+      </form>
+      {formData.loading && <Loader />}
+    </div>
   );
 };
 
--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FaCloudUploadAlt, FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 import { registerReq } from 'api';
 import toast from 'react-hot-toast';
+import Loader from './Loader';
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState({
@@ -81,87 +82,90 @@ const RegisterForm = () => {
   };
 
   return (
-    <form onSubmit={handleLogin} className="space-y-6">
-      <div>
-        <label htmlFor="name" className="block mb-1 font-medium text-gray-900">
-          Full Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          placeholder="Please enter your full name"
-          className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
-          border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 
-          duration-150 ease-linear outline-none rounded-md"
-        />
-      </div>
-      <div>
-        <label htmlFor="email" className="block mb-1 font-medium text-gray-900">
-          Email Address
-        </label>
-        <input
-          type="text"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder="Please enter your email address"
-          className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
-          border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 
-          duration-150 ease-linear outline-none rounded-md"
-        />
-      </div>
-      <div>
-        <label htmlFor="password" className="block mb-1 font-medium text-gray-900">
-          Password
-        </label>
-        <div className="relative">
+    <div>
+      <form onSubmit={handleLogin} className="space-y-6">
+        <div>
+          <label htmlFor="name" className="block mb-1 font-medium text-gray-900">
+            Full Name
+          </label>
           <input
-            type={formData.showPassword ? 'text' : 'password'}
-            name="password"
-            value={formData.password}
+            type="text"
+            name="name"
+            value={formData.name}
             onChange={handleChange}
-            placeholder="Please enter your password"
+            placeholder="Please enter your full name"
             className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
+          border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 
+          duration-150 ease-linear outline-none rounded-md"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="block mb-1 font-medium text-gray-900">
+            Email Address
+          </label>
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Please enter your email address"
+            className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
+          border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 
+          duration-150 ease-linear outline-none rounded-md"
+          />
+        </div>
+        <div>
+          <label htmlFor="password" className="block mb-1 font-medium text-gray-900">
+            Password
+          </label>
+          <div className="relative">
+            <input
+              type={formData.showPassword ? 'text' : 'password'}
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Please enter your password"
+              className="px-4 text-sm w-full h-11 text-gray-700 bg-white placeholder:text-gray-400 placeholder:font-light 
             border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-200 
             duration-150 ease-linear outline-none rounded-md"
-          />
+            />
+            <button
+              type="button"
+              onClick={() => setFormData({ ...formData, showPassword: !formData.showPassword })}
+              className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            >
+              {formData.showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </button>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="avatar" className="block mb-1 font-medium text-gray-900">
+            Avatar
+          </label>
+          <div className="relative px-4 h-16 border-dotted border-2 border-gray-200 rounded-md">
+            <FaCloudUploadAlt className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl text-gray-400" />
+            <input
+              type="file"
+              multiple={false}
+              accept="image/*"
+              onChange={(e) => handleUpload(e.target.files[0])}
+              className="w-full h-full opacity-0 cursor-pointer z-10"
+            />
+          </div>
+          <span className="block mt-2 text-sm text-gray-500 font-light">Only Jpg, Jpeg, Png</span>
+        </div>
+        <div className="">
           <button
-            type="button"
-            onClick={() => setFormData({ ...formData, showPassword: !formData.showPassword })}
-            className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            type="submit"
+            disabled={isUpload}
+            className="inline-block w-full h-11 font-medium rounded-md transition border border-blue-500 text-white bg-blue-600 hover:bg-blue-500 hover:ring-2 hover:ring-blue-200 hover:ring-offset-2"
           >
-            {formData.showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            Register
           </button>
         </div>
-      </div>
-      <div>
-        <label htmlFor="avatar" className="block mb-1 font-medium text-gray-900">
-          Avatar
-        </label>
-        <div className="relative px-4 h-16 border-dotted border-2 border-gray-200 rounded-md">
-          <FaCloudUploadAlt className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl text-gray-400" />
-          <input
-            type="file"
-            multiple={false}
-            accept="image/*"
-            onChange={(e) => handleUpload(e.target.files[0])}
-            className="w-full h-full opacity-0 cursor-pointer z-10"
-          />
-        </div>
-        <span className="block mt-2 text-sm text-gray-500 font-light">Only Jpg, Jpeg, Png</span>
-      </div>
-      <div className="">
-        <button
-          type="submit"
-          disabled={isUpload}
-          className="inline-block w-full h-11 font-medium rounded-md transition border border-blue-500 text-white bg-blue-600 hover:bg-blue-500 hover:ring-2 hover:ring-blue-200 hover:ring-offset-2"
-        >
-          Register
-        </button>
-      </div>
-    </form>
+      </form>
+      {formData.loading && <Loader />}
+    </div>
   );
 };
 
